c24d19ec218bc120ae7ae9747bd3472865aea49a
José Carlos Jr <josecarlosjr.atc@gmail.com>
2024-02-17 20:16:37 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -12,6 +14,8 @@ public class Program {
 		Department dp = new Department(1, "teste");
 		
 		Seller seller = new Seller(1, "José", "jose@gmail.com", new Date(), 3500.0, dp);
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
 		System.out.println(seller);
 		
